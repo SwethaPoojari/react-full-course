1bf39129c76e712e48b2900644fb3c0eaf0a590e
Swetha <poojari.swetha@dxc.com>
2023-07-25 19:10:11 +0530

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "jest": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    settings: {
+        react: {
+            "version": "detect"
+        }
+    },
+    
+    "parserOptions": {
+        "ecamaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "react/react-in-jsx-scope":"off",
+        "indent":["error",4],
+        "linebreak-style": ["error", "unix"],
+        "quotes": ["error" ,"single"],
+        "semi": ["error", "always"],
+        "no-unused-vars": "off",
+        "react/prop-types": "off"
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+//import Header from './header.js';
+ 
+let data=
+[
+    {
+        postId: 1,
+        id: 1,
+        name: 'id labore ex et quam laborum',
+        email: 'Eliseo@gardner.biz',
+        body: 'laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium',
+    },
+    {
+        postId: 1,
+        id: 2,
+        name: 'quo vero reiciendis velit similique earum',
+        email: 'Jayne_Kuhic@sydney.com',
+        body: 'est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et',
+    },
+    {
+        postId: 1,
+        id: 3,
+        name: 'odio adipisci rerum aut animi',
+        email: 'Nikita@garfield.biz',
+        body: 'quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione',
+    },
+    {
+        postId: 1,
+        id: 1,
+        name: 'id labore ex et quam laborum',
+        email: 'Eliseo@gardner.biz',
+        body: 'laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium',
+    },
+    {
+        postId: 1,
+        id: 2,
+        name: 'quo vero reiciendis velit similique earum',
+        email: 'Jayne_Kuhic@sydney.com',
+        body: 'est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et',
+    },
+    {
+        postId: 1,
+        id: 3,
+        name: 'odio adipisci rerum aut animi',
+        email: 'Nikita@garfield.biz',
+        body: 'quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione',
+    },
+];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App=()=>
+{
+    let newArray=data.map((eachObj)=>{
+        const {id,name,email,body} = eachObj;
+        return (
+            <main className='main-container' key={id}>
+                
+                <ul className='ul-constainer'>
+                    <li className='li-container'>
+                        <div className='div-container'>
+                            <h3>{email}</h3>
+                            <h2 className='name-container'>{name}</h2>
 
-export default App;
+                        </div>
+                        <p className='para-cls-container'>{body}
+                        </p>
+                    </li>
+                </ul>
+            </main>
+        );
+    });
+    return (
+        <section className='profile-card'>
+            {newArray};
+        </section>
+    );
+};
+export default App;   
